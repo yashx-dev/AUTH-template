7d616c1c7f23ff4b088c2a3cce4f744d83f28612
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2026-03-07 13:44:07 +0530

    Add user schema with password hashing and JWT token generation

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+const generateToken = (userId) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT secret is not defined in environment variable");
+  }
+  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || "7d",
+  });
+
+  return token;
+};
+export default generateToken;
